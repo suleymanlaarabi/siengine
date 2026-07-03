61dbeb79d980292ce243397604c7e28eae2f5356
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-07-03 10:42:59 +0200

    register transform component

--- src/transform.c
+++ src/transform.c
@@ -1,7 +1,16 @@
 
 #include "engine_internal.h"
 #include "siecs.h"
+#include "siengine.h"
 
 ECS_MODULE_DEFINE(sitransform);
 
-void sitransform_import(ecs_world_t *world, const sitransform_props_t *props) {}
+ECS_COMPONENT_DEFINE(SIPosition2d);
+ECS_COMPONENT_DEFINE(SIScale2d);
+ECS_COMPONENT_DEFINE(SIRotation2d);
+
+void sitransform_import(ecs_world_t *world, const sitransform_props_t *props) {
+    ECS_COMPONENT_REGISTER(world, SIPosition2d);
+    ECS_COMPONENT_REGISTER(world, SIScale2d);
+    ECS_COMPONENT_REGISTER(world, SIRotation2d);
+}
